01a9a858bed823fa1c0eaa4ba20f5604ad2ae8c8
BenMaRe-Dev <b.maguinareyes@gmail.com>
2020-06-25 14:34:57 -0500

    Add Sign up, Sign in & styles

---
+++ script.js
@@ -0,0 +1,19 @@
+// let signUp = document.querySelector(".sign_up_button");
+// let fecht_auth = signUp.onclick;
+
+// async function authValidation() { 
+// const response = await fetch('http//:localhost:3000/login/', {
+//   method: 'POST', 
+//   mode: 'cors',
+//   body: JSON.stringify({
+//     username : 'codeable', 
+//     password : '123456',
+//   }), 
+//   headers: {
+//     'Content-type': 'application/json', 
+//   },
+// });
+// const jsonData = await response.json(); 
+// localStorage,setItem('token', JSON.stringify(jsonData.token));
+// }
+
